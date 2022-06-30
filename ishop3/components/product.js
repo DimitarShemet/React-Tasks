9c0f5af7e8cb778230f9230a796f78cb0c562dcc
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-06-30 22:32:33 +0300

    added task

---
+++ ishop3/components/product.js
@@ -0,0 +1,52 @@
+
+import React from 'react'
+let Product=React.createClass({
+     
+    displayName:"Product",
+  
+    productClicked:function(EO){
+     this.props.cbSelected(this.props.code)
+     console.log(this.props.selectedItem)
+     console.log(this.props.code)
+     
+   
+  },
+    clickReset:function(EO){
+      EO.stopPropagation()
+      var question =confirm('Удалить строку?');
+      if(question===true)
+        this.props.cbDeleted(this.props.code)
+      else{
+        alert("Строка не будет удалена")
+        return
+      }
+     
+     
+    },
+    render:function(){  
+    
+     
+     return React.DOM.tr({onClick:this.productClicked, style:{backgroundColor:(this.props.selectedItem===this.props.code)?'orange':'white'}}, 
+      React.DOM.td({}, this.props.productName),
+      React.DOM.td({}, this.props.price),
+      React.DOM.td({}, this.props.url),
+      React.DOM.td({}, this.props.balance),
+      React.DOM.td({}, React.DOM.input({type:"reset",value:"Delete",onClick:this.clickReset})),
+     
+    );
+    },
+    
+    })
+    
+    export default Product
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
