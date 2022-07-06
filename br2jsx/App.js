f3247bddab223867d501c0d3e9df67115b18dd5d
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-07-06 18:40:12 +0300

    fix keys

--- br2jsx/App.js
+++ br2jsx/App.js
@@ -6,13 +6,9 @@ import Square from './Square.js';
 let text="первый<br>второй<br/>третий<br />последний";
 
 let rightArr=text.replace(/br \//ig, "br").replace(/br\//ig,"br").split("<br>")
-rightArr.splice(1,0,<br></br>)
-rightArr.splice(3,0,<br></br>)
-rightArr.splice(5,0,<br></br>)
- let right=rightArr.map((elem,index)=>{
-  
-
-})
+rightArr.splice(1,0,<br key={1}></br>)
+rightArr.splice(3,0,<br key={2}></br>)
+rightArr.splice(5,0,<br key={3}></br>)
 
 
 
