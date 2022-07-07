8790d6c8d964abc518d10528e6998460aa5955cb
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-07-07 21:50:33 +0300

    fix

--- br2jsx/Square.js
+++ br2jsx/Square.js
@@ -7,9 +7,15 @@ class Square extends React.Component {
     };
 render(){
   let rightArr=this.props.text.replace(/br \//ig, "br").replace(/br\//ig,"br").split("<br>")
-  rightArr.splice(1,0,<br key={Math.random()}></br>)
-  rightArr.splice(3,0,<br key={Math.random()}></br>)
-  rightArr.splice(5,0,<br key={Math.random()}></br>)
+  rightArr.forEach((elem,index)=>{
+    rightArr.splice(1+index*2,0,<br key={index}></br>)
+  })
+  
+  
+  
+  // rightArr.splice(1,0,<br key={Math.random()}></br>)
+  // rightArr.splice(3,0,<br key={Math.random()}></br>)
+  // rightArr.splice(5,0,<br key={Math.random()}></br>)
  
     return (
        
---
+++ errorishop3/App.js
@@ -0,0 +1,12 @@
+﻿import React from "react"
+import ReactDOM from 'react-dom';
+import  Shop from "./components/shop.js"
+
+let shopName="Lamoda"
+let  data=["Name","Price","URL","Quanity","Control"]
+  import productList  from './clothers.json'
+  ReactDOM.render(
+    <Shop shop={shopName} dataItems={data} list={productList}></Shop>,
+      document.getElementById('container')
+      )
+    
